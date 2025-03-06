8f22ee421813ada7d97018ab94af9d1bed549c47
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-03-06 22:13:50 +0100

    Update Pagination.jsx

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,46 +1,46 @@
 import React from "react";
 
-function Pagination({ currentPage, totalPages, onNextPage, onPrevPage }) {
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      onPrevPage();
-    }
-  };
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onNextPage: () => void;
+  onPrevPage: () => void;
+}
 
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      onNextPage();
-    }
-  };
-
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onNextPage,
+  onPrevPage,
+}) => {
   return (
     <div className="flex items-center justify-center gap-12">
       <button
-        onClick={handlePrevPage}
+        onClick={onPrevPage}
         disabled={currentPage === 1}
         className="focus:outline-none disabled:opacity-30"
       >
-        <span
-          href="#"
-          className="hover:text-textSecondary dark:text-white dark:hover:text-textSecondary"
-        >
+        <span className="hover:text-textSecondary dark:text-white dark:hover:text-textSecondary">
           Previous page
         </span>
       </button>
+
+      <span className="text-lg font-semibold dark:text-white">
+        Page {currentPage} of {totalPages}
+      </span>
+
       <button
-        onClick={handleNextPage}
+        onClick={onNextPage}
         disabled={currentPage === totalPages}
         className="focus:outline-none disabled:opacity-30"
       >
-        <span
-          href="#"
-          className="hover:text-textSecondary dark:text-white dark:hover:text-textSecondary"
-        >
+        <span className="hover:text-textSecondary dark:text-white dark:hover:text-textSecondary">
           Next page
         </span>
       </button>
     </div>
   );
-}
+};
 
 export default Pagination;
+  
